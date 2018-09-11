890d22f462fbb08f791a63c2fd2e23f1eec18299
partimer <37935350+partimer@users.noreply.github.com>
2018-09-11 15:49:04 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+/*
+ * Create a a separate Vue -- uncconected to the DOM
+ * to listen to
+ */
+import Vue from 'vue';
+const LogBus = new Vue();
+
+
+// This is the function that generates an appender function
+function VueLogBusAppender(layout, timezoneOffset) {
+    
+    // Define a Vue if LogBus DNE
+    if (typeof LogBus !== 'undefined' ) {
+        LogBus = new Vue();
+    }   
+    
+    // This is the appender function itself
+    const appender = (loggingEvent) => {
+        // emit the log message
+        // use log level as emit channel
+        LogBus.$emit(loggingEvent.level, `${layout(loggingEvent, timezoneOffset)}\n`);
+    };
+
+    // add a shutdown function.
+    appender.shutdown = (done) => {
+        process.stdout.write('', done);
+    };
+
+    return appender;
+}
+
+function configure(config, layouts) {
+  let layout = layouts.colouredLayout;
+  if (config.layout) {
+    layout = layouts.layout(config.layout.type, config.layout);
+  }
+  return VueLogBusAppender(layout, config.timezoneOffset);
+}
+
+exports.configure = configure;
